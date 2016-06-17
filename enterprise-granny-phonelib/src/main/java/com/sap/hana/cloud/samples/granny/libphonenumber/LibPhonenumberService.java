68f9069fdb1acd89a403a5aa63f54800ae6863ab
Matthias Steiner <matthias.steiner@inscope.net>
2016-06-17 19:16:07 +0200

    WIP (libphonenumber service/wrapper)
    Change-Id: I78af5fc604210dfb0d97e1d98c1c66dabceb99fa
    Signed-off-by: Matthias Steiner <matthias.steiner@inscope.net>

---
+++ enterprise-granny-phonelib/src/main/java/com/sap/hana/cloud/samples/granny/libphonenumber/LibPhonenumberService.java
@@ -0,0 +1,125 @@
+package com.sap.hana.cloud.samples.granny.libphonenumber;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.springframework.stereotype.Service;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberToCarrierMapper;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.PhoneNumberUtil.PhoneNumberFormat;
+import com.google.i18n.phonenumbers.PhoneNumberUtil.PhoneNumberType;
+import com.google.i18n.phonenumbers.Phonenumber.PhoneNumber;
+
+/**
+ *  .
+ */
+@Service("libPhonenumberService")
+@Path("/phone")
+@Produces({ "application/json" })
+@com.webcohesion.enunciate.metadata.Facet(value="PhoneNumber Service", documentation="Provides a microservice for Google's libphonenumber library.")
+@com.webcohesion.enunciate.metadata.rs.ResourceLabel(value="PhoneNumber Service")
+@com.webcohesion.enunciate.metadata.rs.ServiceContextRoot(value="/api/v1")
+public class LibPhonenumberService
+{
+	/**
+	 * 
+	 * @param phoneNumber
+	 * @param region Region Code string using ISO 3166-1 two-letter country-code format in upper-case. 
+	 * @param header
+	 * @return
+	 * 
+	 * @see The list of the codes can be found here: http://www.iso.org/iso/country_codes/iso_3166_code_lists/country_names_and_code_elements.htm
+	 */
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@com.webcohesion.enunciate.metadata.rs.TypeHint(PhoneNumberValidationResult.class)
+	@com.webcohesion.enunciate.metadata.rs.StatusCodes(value = {
+			   @com.webcohesion.enunciate.metadata.rs.ResponseCode(code = 200, condition = "In case of success"),
+			   @com.webcohesion.enunciate.metadata.rs.ResponseCode(code = 400, condition = "In case no phonenumber or region code was provided")})
+
+	public Response validate(@QueryParam("phonenumber") String phoneNumber, 
+			                 @QueryParam("region") String region,
+			                 @Context HttpHeaders header) 
+	{	
+		PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
+		
+		PhoneNumber phoneNo = null;
+		
+		PhoneNumberValidationResult result = new PhoneNumberValidationResult();
+		result.setNumber(phoneNumber);
+		
+		try 
+		{
+			Locale locale = null;
+			boolean knownCountry = false;
+			String country = null;
+			
+			if (region != null && ! region.isEmpty())
+			{
+				country = region.toUpperCase();
+			}
+			else
+			{
+				if (header.getLanguage() != null)
+				{
+					country = header.getLanguage().getCountry();
+				}
+			}
+			
+			if (country != null)
+			{
+				knownCountry = Arrays.asList(Locale.getISOCountries()).contains(country);
+			}
+			else
+			{
+				return Response.status(Status.BAD_REQUEST).build();
+			}
+			
+			if (! knownCountry)
+			{
+				// fallback 
+			}
+			
+			phoneNo = phoneUtil.parse(phoneNumber, country);
+			
+			// check for validity
+			boolean isValid = phoneUtil.isValidNumber(phoneNo);
+
+			
+			if (isValid)
+			{
+				PhoneNumberType type = phoneUtil.getNumberType(phoneNo);
+				PhoneNumberToCarrierMapper carrierMapper = PhoneNumberToCarrierMapper.getInstance();
+				
+				String intFormat = phoneUtil.format(phoneNo, PhoneNumberFormat.INTERNATIONAL);
+				String phoneNoType = type.toString().toUpperCase();
+				String carrier = carrierMapper.getNameForNumber(phoneNo, Locale.ENGLISH);
+				
+				result.setValid(isValid);
+				result.setNumber(intFormat);
+				result.setType(phoneNoType);
+				result.setCarrier(carrier);
+			}
+		} 
+		catch (NumberParseException e) 
+		{
+			System.err.println("NumberParseException was thrown: " + e.toString());
+			return Response.status(Status.BAD_REQUEST).entity(e.toString()).build();
+		}
+		
+		return Response.ok(result).build();
+	}
+}
---
+++ enterprise-granny-phonelib/src/main/java/com/sap/hana/cloud/samples/granny/libphonenumber/PhoneNumberValidationResult.java
@@ -0,0 +1,48 @@
+package com.sap.hana.cloud.samples.granny.libphonenumber;
+
+import java.io.Serializable;
+
+public class PhoneNumberValidationResult implements Serializable
+{
+
+	private static final long serialVersionUID = 1L;
+
+	boolean valid = false;
+	
+	String type = null;
+	String number = null;
+	String carrier = null;
+	
+	public boolean isValid()
+	{
+		return valid;
+	}
+	public void setValid(boolean valid)
+	{
+		this.valid = valid;
+	}
+	public String getType()
+	{
+		return type;
+	}
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+	public String getNumber()
+	{
+		return number;
+	}
+	public void setNumber(String number)
+	{
+		this.number = number;
+	}
+	public String getCarrier()
+	{
+		return carrier;
+	}
+	public void setCarrier(String carrier)
+	{
+		this.carrier = carrier;
+	}
+}
